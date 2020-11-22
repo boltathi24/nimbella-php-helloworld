b6f5d0d295357ed7d56cb24c93eb8603c97ac6d9
athithan-7130 <athithan-7130@athithan-7130.csez.zohocorpin.com>
2020-11-22 16:34:56 +0530

    hello world php

---
+++ packages/myapp/helloworld/new.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Checks if a file exists in the data bucket containing
+ * and returns its contents. If not, creates the file and
+ * initializes its contents. The contents are the date "since"
+ * the project was deployed.
+ */
+
+
+function main() : array {
+
+
+    return [ 'body' => 'hello world' ];
+  
+}
